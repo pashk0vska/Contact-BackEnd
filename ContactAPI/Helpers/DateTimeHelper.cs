74ca208310b068894c2ec76753b36af4a89bdaa6
fukkut <svyatoslav.luchko@kpk-lp.com.ua>
2026-03-10 16:20:20 +0200

    Add DateTime normalization helper methods

--- ContactAPI/Helpers/DateTimeHelper.cs
+++ ContactAPI/Helpers/DateTimeHelper.cs
@@ -1,1 +1,25 @@
+using System;
+
+namespace Contact.API.Helpers
+{
+   
+    public static class DateTimeHelper
+    {
+        
+        public static DateTime NormalizeToUtc(DateTime dt)
+        {
+            return dt.Kind switch
+            {
+                DateTimeKind.Utc => dt,
+                DateTimeKind.Local => dt.ToUniversalTime(),
+                _ => DateTime.SpecifyKind(dt, DateTimeKind.Local).ToUniversalTime(),
+            };
+        }
+
+        public static DateTime NormalizeOrNow(DateTime dt)
+        {
+            return dt == default ? DateTime.UtcNow : NormalizeToUtc(dt);
+        }
+    }
+}
 
